3048b9913dc6acd70a9ab90fac651d233821f5d7
JoseFausti <141734639+JoseFausti@users.noreply.github.com>
2023-09-19 09:47:25 -0300

    Ejercicio_Funciones.py

--- Ejercicio_Funciones.py
+++ Ejercicio_Funciones.py
@@ -1,7 +1,7 @@
 """1)_El siguiente programa debería imprimir el número 2 si se le ingresan como 
     valores x=5, y=1 pero en su lugar imprime 5. ¿Qué hay que corregir?""" 
 
-# Lo qu hay que corregir es dentro de la funcion. Nos devuelve el valor de las
+# Lo que hay que corregir es dentro de la funcion. Nos devuelve el valor de las
 # variables la cual no se modifican y lo que realmente debe devolver son los 
 # valores de los parametros... El codigo quedaria de la siguiente forma:
 
