6827b96f6c07a7298434e2da97cb4ca92e698910
MaxonHLP <141735104+MaxonHLP@users.noreply.github.com>
2023-10-26 09:47:04 -0300

    Update Trabajo_Práctico_8_Programacion_1.py

--- Trabajo_Práctico_8_Programacion_1.py
+++ Trabajo_Práctico_8_Programacion_1.py
@@ -1,3 +1,5 @@
+from gettext import find
+import random
 """1.	Escribir una función que reciba un número positivo n y devuelva la cantidad de dígitos que tiene."""
 def f(n):
     s=str(n)
@@ -30,6 +32,51 @@ if f(n,b) == True:
     print("n es potencia de b")
 else:
     print("n no es potencia de b")
+
+
+# # 3-Escribir una funcion recursiva que reciba como parámetros dos strings a y b, 
+# # y devuelva una lista con las posiciones en donde se encuentra b dentro de a. Ejemplo:
+
+
+def internity(a, b, c=[], start=0):
+     aux_1 = a.find(b, start)
+     if aux_1 != -1:
+         c.append(aux_1)
+         return internity(a, b, c, aux_1 + 1)
+     else:
+         return c
+
+print(internity("Un tete a tete con Tete", "te"))
+
+#  -4.	Escribir dos funciones mutualmente recursivas par(n) e impar(n) que determinen la paridad del numero natural dado, conociendo solo que:
+#  •	1 es impar.
+#  •	Si un número es impar, su antecesor es par; y viceversa.
+
+def impar (number):
+    
+    if number == 1 :   
+        return 1
+    elif number == 0 :
+        return 0
+    else :
+        if impar(number-2) == 1:
+            return True
+        
+if impar(random.randint(1 , 10)) :
+    print("el numero es impar !!!!!!!!!") 
+
+def par (number):
+    
+     if number == 1 :
+         return 1
+     elif number == 0 :
+         return 0
+     else :
+         if par(number-2) == 0:
+             return True
+if par(random.randint(1 , 10)) :
+    print("el numero es par !!!!!!!!!")      
+
 # 7)_Implemente un algoritmo, usando una función recursiva, que resuelva la siguiente sumatoria:
 # K(n, p) = p + 2 ∗ p + 3 ∗ p + 4 ∗ p + … + n ∗ p
 # El programa debe pedir al usuario que ingrese un número n, y un número d,
